95380ec043dd67374fe8ae8cbe812b721e800598
alinouri76 <alinouri@live.cn>
2020-03-06 01:09:35 -0500

    data cleaning skeleton

---
+++ clean_data.py
@@ -0,0 +1,27 @@
+
+def remove_non_nba():
+    # regular season match ids start with 2
+    # Filter all those starting with 2
+
+
+def separate_season():
+    # The data contains 2 seasons
+    # Separate 2017 and 2018 season
+    # Look at season_id attribute
+
+
+
+def bool_home_away():
+    # Use the players dataset
+    #Compare team_id with hometeam_id
+    # Then use match_ids in the two datasets to take home/away boolean
+    # from players dataset to team dataset
+
+
+def pts_scored_received():
+    # distinguish between  points scored and points received
+    # look at final_hscore (final home score) and final_vscore
+    # Do bool_home_away frist. That will help you filter out 
+    #home and away scores
+
+
